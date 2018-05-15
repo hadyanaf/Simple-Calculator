e263e564117fa834d808f29f244037ec1e377347
hadyanaf <hadyanaf@gmail.com>
2018-05-15 23:33:28 +0700

    finish project

---
+++ javascript/operator.js
@@ -0,0 +1,109 @@
+function checkBoxEventSum() {
+	var arrIn = [];
+	var el;
+	var prefixCb = "cb";
+	var prefixIn = "input"
+	var res = 0;
+
+	for (var i = 1; el = document.getElementById(prefixCb + i); i++) {
+		if (document.getElementById(prefixCb + i).checked) {
+			arrIn.push(parseInt(document.getElementById(prefixIn + i).value));
+		}
+	}
+
+	if (arrIn.length > 1) {
+		for (var i = 0; i < arrIn.length; i++) {
+				res += arrIn[i];
+		}
+		console.log(res);
+	} else {
+		alert("Error!\nYou must checklist of minimum 2 check-boxes");
+	}
+
+	console.log(arrIn);
+	document.getElementById("total").value = res;
+}
+
+function checkBoxEventSubs() {
+	var arrIn = [];
+	var el;
+	var prefixCb = "cb";
+	var prefixIn = "input"
+	var res = 0;
+	var temp = 0;
+
+	for (var i = 1; el = document.getElementById(prefixCb + i); i++) {
+		if (document.getElementById(prefixCb + i).checked) {
+			arrIn.push(parseInt(document.getElementById(prefixIn + i).value));
+		}
+	}
+
+	if (arrIn.length > 1) {
+		res = arrIn[0];
+		for (var i = 1; i < arrIn.length; i++) {
+				res -= arrIn[i];
+		}
+		console.log(res);
+	} else {
+		alert("Error!\nYou must checklist of minimum 2 check-boxes");
+	}
+
+	console.log(arrIn);
+	document.getElementById("total").value = res;
+}
+
+function checkBoxEventMult() {
+	var arrIn = [];
+	var el;
+	var prefixCb = "cb";
+	var prefixIn = "input"
+	var res = 0;
+	var temp = 0;
+
+	for (var i = 1; el = document.getElementById(prefixCb + i); i++) {
+		if (document.getElementById(prefixCb + i).checked) {
+			arrIn.push(parseInt(document.getElementById(prefixIn + i).value));
+		}
+	}
+
+	if (arrIn.length > 1) {
+		res = arrIn[0];
+		for (var i = 1; i < arrIn.length; i++) {
+				res *= arrIn[i];
+		}
+		console.log(res);
+	} else {
+		alert("Error!\nYou must checklist of minimum 2 check-boxes");
+	}
+
+	console.log(arrIn);
+	document.getElementById("total").value = res;
+}
+
+function checkBoxEventDiv() {
+	var arrIn = [];
+	var el;
+	var prefixCb = "cb";
+	var prefixIn = "input"
+	var res = 0;
+	var temp = 0;
+
+	for (var i = 1; el = document.getElementById(prefixCb + i); i++) {
+		if (document.getElementById(prefixCb + i).checked) {
+			arrIn.push(parseInt(document.getElementById(prefixIn + i).value));
+		}
+	}
+
+	if (arrIn.length > 1) {
+		res = arrIn[0];
+		for (var i = 1; i < arrIn.length; i++) {
+				res /= arrIn[i];
+		}
+		console.log(res);
+	} else {
+		alert("Error!\nYou must checklist of minimum 2 check-boxes");
+	}
+
+	console.log(arrIn);
+	document.getElementById("total").value = res.toFixed(6);
+}
